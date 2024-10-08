25ead648e6e5315d782468858e96c1b729cd5e9e
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-08 20:26:19 +0600

    Project has been created and setup successfully

---
+++ src/app/found-items/page.tsx
@@ -0,0 +1,11 @@
+import { title } from "@/src/components/primitives";
+
+const FoundItemsPage = () => {
+    return (
+        <div>
+            <h1 className={title()}>Found Items Page</h1>
+        </div>
+    );
+};
+
+export default FoundItemsPage;
