246170ce3772f678b17afbd2801e7375b6503a38
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-08 22:11:39 +0600

    Added 404 Not Found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col items-center min-h-screen justify-center gap-2">
+      <h1 className="text-5xl font-bold">404</h1>
+      <h1 className="font-medium">Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
