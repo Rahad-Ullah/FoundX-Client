5d36852160f0bb9cd728e44b9c8b5c8336a88038
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-10 23:02:12 +0600

    Skeleton for recent posts has been implemented

--- src/app/(WithCommonLayout)/(home)/@recentPosts/loading.tsx
+++ src/app/(WithCommonLayout)/(home)/@recentPosts/loading.tsx
@@ -1,9 +1,26 @@
-const Loading = () => {
+import { subtitle, title } from "@/src/components/primitives";
+import CardSkeleton from "@/src/components/UI/CardSkeleton";
+import Containter from "@/src/components/UI/Containter";
+
+const loading = async () => {
   return (
     <div>
-      <h1 className="text-center text-xl font-bold my-10">Loading...</h1>
+      <Containter>
+        <div className="text-center mb-10">
+          <h1 className={title()}>Recent Posts</h1>
+          <p className={subtitle()}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+            harum?
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {[...Array(9)].map(() => (
+            <CardSkeleton />
+          ))}
+        </div>
+      </Containter>
     </div>
   );
 };
 
-export default Loading;
+export default loading;
